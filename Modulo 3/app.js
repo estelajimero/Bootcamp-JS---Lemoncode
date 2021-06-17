52fca537091d26a5fb3928ef1fabb79c32219b7c
Estela <estelajimero@users.noreply.github.com>
2021-06-17 10:42:18 +0200

    first commit

---
+++ Modulo 3/app.js
@@ -0,0 +1,7 @@
+const product = { count: 3, price: 12.55, type: "ropa" };
+
+// Calcular el total
+let total = product.count > 0 ? product.count * product.price : 0;
+console.log(`El total es ${total}`);
+
+// Calcular el IVA
