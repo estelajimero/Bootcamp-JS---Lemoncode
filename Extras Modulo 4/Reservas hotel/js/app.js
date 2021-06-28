f9395194ba8f5e90affdee2bfb34089b785d0d8e
Estela <estelajimero@users.noreply.github.com>
2021-06-28 13:03:18 +0200

    Update ongoing total without button

--- Extras Modulo 4/Reservas hotel/js/app.js
+++ Extras Modulo 4/Reservas hotel/js/app.js
@@ -1,9 +1,59 @@
-let totalBooking = 0;
+function finalPrice() {
+    let roomType = document.getElementById("room-type-input").value; 
+    let roomPrice = 0;
+    
+    switch (roomType) {
+        case "standard":
+            roomPrice = 100;
+            break;
+        
+        case "junior":
+            roomPrice = 120;
+            break;
+        
+        case "suite":
+            roomPrice = 150;
+            break;
+    }
 
-let standardRoom = 100;
-let juniorSuite = 120;
-let suite = 150;
+    let numberNights = parseInt(document.getElementById("nights-number").value);
 
-let spaRate = 20;
-let parkingNight = 10;
+    if(document.getElementById("spa-included").checked) {
+        roomPrice += 20;
+    }
+    
+    let roomCapacity = document.getElementById("room-capacity").value;
+    let capacityRatio = 0;
 
+    switch (roomCapacity) {
+        case "single":
+            capacityRatio = 0.75;
+            break;
+        
+        case "double":
+            capacityRatio = 1;
+            break;
+        
+        case "triple":
+            capacityRatio = 1.25;
+            break;
+    }
+
+    let parkingNights = parseInt(document.getElementById("parking-nights").value);
+    let parkingPrice = 10 * parkingNights;
+
+    let total = "El precio total de la reserva es " + (((roomPrice * capacityRatio) * numberNights) + parkingPrice) + "€";
+
+    document.getElementById("final-price").innerText = total;
+}
+
+// document.getElementById("total").addEventListener("click", finalPrice);
+
+document.getElementById("room-type-input").addEventListener("change", finalPrice);
+document.getElementById("spa-included").addEventListener("change", finalPrice);
+document.getElementById("no-spa").addEventListener("change", finalPrice);
+document.getElementById("room-capacity").addEventListener("change", finalPrice);
+document.getElementById("nights-number").addEventListener("change", finalPrice);
+document.getElementById("parking-nights").addEventListener("change", finalPrice);
+
+finalPrice();
