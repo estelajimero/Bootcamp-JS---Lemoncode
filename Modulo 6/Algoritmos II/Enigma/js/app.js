08410d4736f715b4603e0f1c8e2da5a8e64c5711
Estela <estelajimero@users.noreply.github.com>
2021-07-16 11:22:41 +0200

    Update with indexOf

--- Modulo 6/Algoritmos II/Enigma/js/app.js
+++ Modulo 6/Algoritmos II/Enigma/js/app.js
@@ -4,18 +4,48 @@ let encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm  ñ";
 let encryptButton = document.getElementById("encrypt");
 let decryptButton = document.getElementById("decrypt");
 
+// function encrypt (text) {
+//     let encryptedResult = "";
+//     text = text.toLowerCase();
+
+//     for (let i = 0; i < text.length; i++) {
+//         for (let j = 0; j < plainAlphabet.length; j++) {
+//             if (text[i] === plainAlphabet[j]) {
+//                 encryptedResult += encryptedAlphabet[j];
+
+//                 break;
+//             }
+//         }
+//     }
+
+//     return encryptedResult;
+// }
+
+// function decrypt (text) {
+//     let decryptedResult = "";
+//     text = text.toLowerCase();
+
+//     for (let i = 0; i < text.length; i++) {
+//         for (let j = 0; j < encryptedAlphabet.length; j++) {
+//             if (text[i] === encryptedAlphabet[j]) {
+//                 decryptedResult += plainAlphabet[j];
+
+//                 break;
+//             }
+//         }
+//     }
+
+//     return decryptedResult;
+// }
+
+// Con indexOf
 function encrypt (text) {
     let encryptedResult = "";
     text = text.toLowerCase();
 
     for (let i = 0; i < text.length; i++) {
-        for (let j = 0; j < plainAlphabet.length; j++) {
-            if (text[i] === plainAlphabet[j]) {
-                encryptedResult += encryptedAlphabet[j];
-
-                break;
-            }
-        }
+        let index = plainAlphabet.indexOf(text[i]);
+        encryptedResult += encryptedAlphabet[index];
     }
 
     return encryptedResult;
@@ -26,17 +56,12 @@ function decrypt (text) {
     text = text.toLowerCase();
 
     for (let i = 0; i < text.length; i++) {
-        for (let j = 0; j < encryptedAlphabet.length; j++) {
-            if (text[i] === encryptedAlphabet[j]) {
-                decryptedResult += plainAlphabet[j];
-
-                break;
-            }
-        }
+        let index = encryptedAlphabet.indexOf(text[i]);
+        decryptedResult += plainAlphabet[index];
     }
 
     return decryptedResult;
-}
+}   
 
 let getEncryptedText = () => {
     let encryptedText = document.getElementById("encrypt-text").value;
