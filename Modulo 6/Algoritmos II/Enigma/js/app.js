b6b2e949c1efc5f8da767467b52596abfc1e4e89
Estela <estelajimero@users.noreply.github.com>
2021-07-16 10:18:31 +0200

    First attemp with iteration

--- Modulo 6/Algoritmos II/Enigma/js/app.js
+++ Modulo 6/Algoritmos II/Enigma/js/app.js
@@ -1,2 +1,56 @@
-let plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
-let encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+let plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,' ";  
+let encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm ";
+
+let encryptButton = document.getElementById("encrypt");
+let decryptButton = document.getElementById("decrypt");
+
+function encrypt (text) {
+    let encryptedResult = "";
+
+    for (let i = 0; i < text.length; i++) {
+        for (let j = 0; j < plainAlphabet.length; j++) {
+            if (text[i] === plainAlphabet[j]) {
+                encryptedResult += encryptedAlphabet[j];
+
+                break;
+            }
+        }
+    }
+
+    return encryptedResult;
+}
+
+function decrypt (text) {
+    let decryptedResult = "";
+
+    for (let i = 0; i < text.length; i++) {
+        for (let j = 0; j < encryptedAlphabet.length; j++) {
+            if (text[i] === encryptedAlphabet[j]) {
+                decryptedResult += plainAlphabet[j];
+
+                break;
+            }
+        }
+    }
+
+    return decryptedResult;
+}
+
+let getEncryptedText = () => {
+    let encryptedText = document.getElementById("encrypt-text").value;
+
+    let textResult = encrypt(encryptedText);
+
+    document.getElementById("decrypt-text").value = textResult;
+}
+
+let getDecryptedText = () => {
+    let decryptedText = document.getElementById("encrypt-text").value;
+
+    let textResult = decrypt(decryptedText);
+
+    document.getElementById("decrypt-text").value = textResult;
+}
+
+encryptButton.addEventListener('click', getEncryptedText);
+decryptButton.addEventListener('click', getDecryptedText);
